69db7a47c02a2c55b2e826110de33eba41a8ad79
Hooahclitus <63759520+Hooahclitus@users.noreply.github.com>
2023-09-28 15:21:49 -0700

    cleans up code for the closest_destination and return_to_hub functions

--- navigation.py
+++ navigation.py
@@ -6,7 +6,12 @@ location_data = create_location_data('data/location_data.csv')
 def closest_destination(truck):
     destinations_from_location = location_data.get(truck.get('location'))
     package_destinations = [package.get('addr') for package in truck.get('cargo') if package.get('status') != 'Delivered']
-    valid_destinations = [(destination, distance) for destination, distance in destinations_from_location.items() if destination in package_destinations]
-    min_distance = min(valid_destinations, key=lambda kv: kv[1])
+    
+    valid_destinations = {dest: dist for dest, dist in destinations_from_location.items() if dest in package_destinations}
+    min_address = min(valid_destinations, key=valid_destinations.get)
+    
+    return {min_address: valid_destinations[min_address]}
 
-    return min_distance
+        
+def return_to_hub(truck):
+    return {'4001 South 700 East': location_data.get(truck.get('location')).get('4001 South 700 East')}
