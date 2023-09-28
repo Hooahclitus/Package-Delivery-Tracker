7c6a128c44bea9668e6abf1868106a5e390604cb
Hooahclitus <63759520+Hooahclitus@users.noreply.github.com>
2023-09-28 12:44:30 -0700

    updates function calls to get_value within navigation.py and truck.py to reflect changes to hash_table.py get function

--- navigation.py
+++ navigation.py
@@ -1,6 +1,12 @@
-def shortest_destination(truck, location_data):
-    destinations_from_location = location_data.get(truck.get_value('location'))
-    package_destinations = [package.get_value('addr') for package in truck.get_value('cargo')]
+from csv_parser import *
+
+
+location_data = create_location_data('data/location_data.csv')
+
+# todo: rename to closest_destination
+def shortest_destination(truck):
+    destinations_from_location = location_data.get(truck.get('location'))
+    package_destinations = [package.get('addr') for package in truck.get('cargo')]
     valid_destinations = [(destination, distance) for destination, distance in destinations_from_location.items() if destination in package_destinations]
     min_distance = min(valid_destinations, key=lambda kv: kv[1])
 
--- truck.py
+++ truck.py
@@ -1,10 +1,12 @@
 from hash_table import *
+from navigation import *
 
+# todo: remove destination if not needed
 def create_truck(cargo=None):
     truck = HashTable(6, 
         'hub', '4001 South 700 East',
         'location', '4001 South 700 East', 
-        'destination', None,
+        # 'destination', None,
         'depart_time', None,
         'arrive_time', None,
         'cargo', cargo,
@@ -13,13 +15,23 @@ def create_truck(cargo=None):
     return truck
     
 def load_cargo(truck, cargo):
-    truck.insert_item('cargo', cargo)
+    truck.insert('cargo', cargo)
     return truck
 
 def deliver_packages(truck):
-    for package in truck.get_value('cargo'):
-        if package.get_value('addr') == truck.get_value('location'):
+    for package in truck.get('cargo'):
+        if package.get('addr') == truck.get('location'):
             package.update('status', 'Delivered')
+    return truck
 
 def update_distance_traveled(truck, distance):
-    truck.update('distance', truck.get_value('distance') + distance)
+    truck.update('distance', truck.get('distance') + distance)
+    return truck
+
+# def change_position(truck):
+#     destination = shortest_destination(truck)
+#     destination_address = destination[0]
+#     distance = destination[1]
+
+#     truck.update('location', destination_address, 'destination', None, 'distance', truck.get_value('distance') + distance)
+#     return truck
