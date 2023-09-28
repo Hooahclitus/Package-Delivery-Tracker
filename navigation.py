8baa4cb4d326063e095309d401ddff72b0de6f71
Hooahclitus <63759520+Hooahclitus@users.noreply.github.com>
2023-09-28 12:49:13 -0700

    updates shortest_destination such that it only includes packages with status not set to Delivered

--- navigation.py
+++ navigation.py
@@ -3,10 +3,9 @@ from csv_parser import *
 
 location_data = create_location_data('data/location_data.csv')
 
-# todo: rename to closest_destination
 def shortest_destination(truck):
     destinations_from_location = location_data.get(truck.get('location'))
-    package_destinations = [package.get('addr') for package in truck.get('cargo')]
+    package_destinations = [package.get('addr') for package in truck.get('cargo') if package.get('status') != 'Delivered']
     valid_destinations = [(destination, distance) for destination, distance in destinations_from_location.items() if destination in package_destinations]
     min_distance = min(valid_destinations, key=lambda kv: kv[1])
 
