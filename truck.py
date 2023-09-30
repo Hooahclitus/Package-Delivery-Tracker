b9bce7023f286d517b5d42d87a56d591e7a3d7d9
Hooahclitus <63759520+Hooahclitus@users.noreply.github.com>
2023-09-29 19:52:11 -0700

    updates truck.py so that any reference to time is time() and not datetime

--- truck.py
+++ truck.py
@@ -6,8 +6,8 @@ from datetime import datetime, date, time, timedelta
 def create_truck(cargo=None, time=time(8, 00)):
     truck = HashTable(6, 
         'location', '4001 South 700 East', 
-        'arrive_time', datetime.combine(date.today(), time),
-        'depart_time', datetime.combine(date.today(), time),
+        'arrive_time', time,
+        'depart_time', time,
         'cargo', cargo,
         'distance', 0,
         'log', []
@@ -30,7 +30,7 @@ def deliver_packages(truck):
     for index, package in enumerate(truck.get('cargo')):
         if package.get('addr') == truck.get('location'):
             unload_cargo(truck, index) 
-            log_cargo(truck, f"ID: {package.get('id')}, ADDRESS: {package.get('addr')}, STATUS: Delivered, TIME: {truck.get('arrive_time').time()}")
+            log_cargo(truck, f"ID: {package.get('id')}, ADDRESS: {package.get('addr')}, STATUS: Delivered, TIME: {truck.get('arrive_time')}")
     return truck
 
 def update_distance_traveled(truck, distance):
@@ -42,7 +42,7 @@ def update_location(truck, address):
     return truck
 
 def update_arrive_time(truck, distance):
-    truck.update('arrive_time', truck.get('depart_time') + timedelta(seconds=distance_to_seconds(distance)))
+    truck.update('arrive_time', (datetime.combine(date.today(), truck.get('depart_time')) + timedelta(seconds=distance_to_seconds(distance))).time())
     return truck
 
 def update_depart_time(truck):
