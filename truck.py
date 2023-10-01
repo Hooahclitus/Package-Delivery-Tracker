b6027ad3a11896cfdb3a9b016209b0bf321e2ed2
Hooahclitus <63759520+Hooahclitus@users.noreply.github.com>
2023-09-30 18:57:50 -0700

    overhauled functions to so that cargo works with a dictionary which is the result of the group_by_address function. updates log_cargo and unload_cargo. adds dump cargo. misc formatting changes throughout.

--- truck.py
+++ truck.py
@@ -1,50 +1,61 @@
 from utils import *
+from package import *
 from hash_table import *
 from datetime import datetime, date, time, timedelta
 
-# todo: remove destination if not needed
+
 def create_truck(cargo=None, time=time(8, 00)):
-    truck = HashTable(6, 
-        'location', '4001 South 700 East', 
-        'arrive_time', time,
-        'depart_time', time,
-        'cargo', cargo,
-        'distance', 0,
-        'log', []
+    truck = HashTable(6,
+        "location", "4001 South 700 East",
+        "arrive_time", time,
+        "depart_time", time,
+        "cargo", cargo,
+        "distance", 0,
+        "log", [],
     )
     return truck
-    
+
+
 def load_cargo(truck, cargo):
-    truck.insert('cargo', cargo)
+    truck.insert("cargo", cargo)
     return truck
 
-def unload_cargo(truck, index):
-    truck.get('cargo').pop(index)
+
+def unload_cargo(truck, address):
+    truck.get("cargo").pop(address)
     return truck
 
-def log_cargo(truck, entry):
-    truck.get('log').append(entry)
+
+def dump_cargo(truck):
+    truck.get("cargo").clear()
     return truck
 
-def deliver_packages(truck):
-    for index, package in enumerate(truck.get('cargo')):
-        if package.get('addr') == truck.get('location'):
-            unload_cargo(truck, index) 
-            log_cargo(truck, f"ID: {package.get('id')}, ADDRESS: {package.get('addr')}, STATUS: Delivered, TIME: {truck.get('arrive_time')}")
+
+def log_cargo(truck, *packages):
+    for package in packages:
+        truck.update("log", truck.get("log") + package)
     return truck
+
 
 def update_distance_traveled(truck, distance):
-    truck.update('distance', truck.get('distance') + distance)
+    truck.update("distance", truck.get("distance") + distance)
     return truck
+
 
 def update_location(truck, address):
-    truck.update('location', address)
+    truck.update("location", address)
     return truck
+
 
 def update_arrive_time(truck, distance):
-    truck.update('arrive_time', (datetime.combine(date.today(), truck.get('depart_time')) + timedelta(seconds=distance_to_seconds(distance))).time())
+    truck.update(
+        "arrive_time", (
+            datetime.combine(date.today(), 
+                truck.get("depart_time")) + timedelta(seconds=distance_to_seconds(distance))
+            ).time())
     return truck
 
+
 def update_depart_time(truck):
-    truck.update('depart_time', truck.get('arrive_time'))
+    truck.update("depart_time", truck.get("arrive_time"))
     return truck
