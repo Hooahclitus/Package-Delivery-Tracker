c7885311d87daf2ed72eaa50ea587b6764459d73
Hooahclitus <63759520+Hooahclitus@users.noreply.github.com>
2023-10-05 12:17:26 -0700

    updates distance_to_seconds and adds new functions

--- utils.py
+++ utils.py
@@ -1,2 +1,17 @@
+from datetime import datetime, date, timedelta
+
+
 def distance_to_seconds(distance):
-    return distance / (18 / 60) * 60
+    return distance * 200
+
+def seconds_to_distance(seconds):
+    return seconds * 0.005
+
+def difference_of_times(time_1, time_2):
+    t1 = datetime.combine(date.today(), time_1)
+    t2 = datetime.combine(date.today(), time_2)
+
+    t1_delta = timedelta(hours=t1.hour, minutes=t1.minute, seconds=t1.second)
+    t2_delta = timedelta(hours=t2.hour, minutes=t2.minute, seconds=t2.second)
+    
+    return t2_delta - t1_delta
