b324fee9fe78c62e6c19c8dabafeae9a7e9aad5c
Hooahclitus <63759520+Hooahclitus@users.noreply.github.com>
2023-10-05 12:14:24 -0700

    changes package.get(addr) to .get(address). combines update_package_time and update_package_status

--- package.py
+++ package.py
@@ -1,7 +1,7 @@
 def group_by_address(packages):
     grouped = {}
     for package in packages:
-        address = package.get('addr')
+        address = package.get('address')
         if address in grouped:
             grouped[address].append(package)
         else:
@@ -9,10 +9,7 @@ def group_by_address(packages):
     
     return grouped
 
-def update_package_status(packages, status):
+def update_status_and_time(packages, status, time):
     for package in packages:
         package.assoc('status', status)
-
-def update_package_time(packages, time):
-    for package in packages:
-        package.assoc('time, str(time)')
+        package.assoc('delivery_time', str(time))
