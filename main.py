ea5e4da80c893693bd2bd2362694b7327b391397
Hooahclitus <63759520+Hooahclitus@users.noreply.github.com>
2023-10-06 10:54:12 -0700

    updates main.py to include outputting for trucks 1-3 and includes correct package IDs for each truck

--- main.py
+++ main.py
@@ -6,14 +6,14 @@ from datetime import time
 
 package_data = create_package_data('data/package_data.csv')
 
-tk_1 = package_data.get(*range(1, 17))
+tk_1 = package_data.get(*[10, 11, 12, 13, 14, 15, 16, 17, 19, 20, 21, 22, 23, 24, 34, 39])
 truck_1 = create_truck(tk_1)
 
-# tk_2 = package_data.get(*range(17, 33))
-# truck_2 = create_truck(tk_2)
+tk_2 = package_data.get(*[1, 2, 3, 4, 5, 7, 18, 27, 29, 30, 33, 35, 36, 37, 38, 40])
+truck_2 = create_truck(tk_2)
 
-# tk_3 = package_data.get(*range(33, 41))
-# truck_3 = create_truck(tk_3)
+tk_3 = package_data.get(*[6, 8, 9, 25, 26, 28, 31, 32])
+truck_3 = create_truck(tk_3)
 
 
 def process_deliveries(truck, start_time=time(8), end_time=None):
@@ -41,9 +41,22 @@ def process_deliveries(truck, start_time=time(8), end_time=None):
     return_to_hub(truck)
     return truck
 
-process_deliveries(truck_1, start_time=time(8), end_time=time(9,10))
-
+process_deliveries(truck_1)
 for package in truck_1.get('log'):
-    print(f"ID: {package.get('id'):4} STATUS: {package.get('status'):12} TIME: {package.get('delivery_time'):10}")
+    print(f"ID: {package.get('id'):4} ADDRESS: {package.get('address')[:30]:30} STATUS: {package.get('status'):12} TIME: {package.get('delivery_time'):10} DEADLINE: {package.get('has_deadline'):4}")
+print(f"Distance Traveled: {round(truck_1.get('distance'), 2):<10} ARRIVAL TIME: {truck_1.get('arrive_time')}")
 
-print(f"Distance Traveled: {round(truck_1.get('distance'), 2):<10}")
+
+process_deliveries(truck_2)
+for package in truck_2.get('log'):
+    print(f"ID: {package.get('id'):4} ADDRESS: {package.get('address')[:30]:30} STATUS: {package.get('status'):12} TIME: {package.get('delivery_time'):10} DEADLINE: {package.get('has_deadline'):4}")
+print(f"Distance Traveled: {round(truck_2.get('distance'), 2):<10} ARRIVAL TIME: {truck_2.get('arrive_time')}")
+
+
+process_deliveries(truck_3, start_time=time(9,45))
+for package in truck_3.get('log'):
+    print(f"ID: {package.get('id'):4} ADDRESS: {package.get('address')[:30]:30} STATUS: {package.get('status'):12} TIME: {package.get('delivery_time'):10} DEADLINE: {package.get('has_deadline'):4}")
+print(f"Distance Traveled: {round(truck_3.get('distance'), 2):<10} ARRIVAL TIME: {truck_3.get('arrive_time')}")
+
+
+round(sum([truck.get('distance') for truck in [truck_1, truck_2, truck_3]]), 2)
