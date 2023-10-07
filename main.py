1149d50374ddac3110088fb675b42b04f88947a5
Hooahclitus <63759520+Hooahclitus@users.noreply.github.com>
2023-10-06 19:07:54 -0700

    further cleans up and reduces redundant code

--- main.py
+++ main.py
@@ -24,25 +24,27 @@ def process_deliveries(truck, end_time):
             case _:
                 deliver_packages(truck, address, distance, 'Delivered')
     return_to_hub(truck)
+
     return truck
 
 def initialize_and_process_trucks(end_time):
     package_data = create_package_data('data/package_data.csv')
 
-    truck_1_packages = package_data.get(*[1, 4, 6, 7, 25, 26, 28, 29, 30, 31, 32, 40])
-    truck_2_packages = package_data.get(*[3, 5, 13, 14, 15, 16, 18, 19, 20, 21, 34, 36, 37, 38, 39])
-    truck_3_packages = package_data.get(*[2, 8, 9, 10, 11, 12, 17, 22, 23, 24, 27, 33, 35])
+    cargo_1 = package_data.get(*[1, 4, 6, 7, 25, 26, 28, 29, 30, 31, 32, 40])
+    cargo_2 = package_data.get(*[3, 5, 13, 14, 15, 16, 18, 19, 20, 21, 34, 36, 37, 38, 39])
+    cargo_3 = package_data.get(*[2, 8, 9, 10, 11, 12, 17, 22, 23, 24, 27, 33, 35])
 
-    for package in truck_3_packages:
+    for package in cargo_3:
         if package.get('id') == '9':
             package.assoc('address', '410 S State St')
 
-    truck_1 = create_truck(truck_1_packages, depart_time=time(9, 5))
-    truck_2 = create_truck(truck_2_packages, depart_time=time(8))
-    truck_3 = create_truck(truck_3_packages, depart_time=time(10,20))
+    cargo_list = [cargo_1, cargo_2, cargo_3]
+    truck_times = [time(9, 5), time(8), time(10,20)]
 
-    for truck in [truck_1, truck_2, truck_3]:
-        process_deliveries(truck, end_time)
+    truck_1, truck_2, truck_3 = [
+        process_deliveries(create_truck(cargo, depart_time=time), end_time)
+        for cargo, time in zip(cargo_list, truck_times)
+    ]
 
     return truck_1, truck_2, truck_3
 
@@ -63,4 +65,4 @@ def main(end_time=None):
 
     print(f"COMBINED DISTANCE TRAVELED: {round(sum([truck.get('distance') for truck in [truck_1, truck_2, truck_3]]), 2)}")
 
-main(time(9,35))
+main()
